33d3c9b1fefc00c66b891a1398af61534de1a64d
Cássia Maués Cuóco <67009042+ccmaues@users.noreply.github.com>
2024-04-22 14:59:19 -0300

    Create Figure4.R

---
+++ Figure4.R
@@ -0,0 +1,80 @@
+# PRS distribuition plot (case x control)
+source("functions_to_source.R")
+
+# Phenotype data
+pADHD <-
+select(pheno, IID, wave, dcanyhk) %>%
+rename(diagnosis = dcanyhk)
+
+# Age data
+vADHD <-
+select(prs_v2, IID, ADHD) %>%
+rename(PRS = ADHD)
+
+## Working data
+# Overall
+data <-
+plyr::join_all(
+  list(pADHD, vADHD),
+  by = "IID",
+  type = "inner") %>%
+  mutate(
+    diagnosis = case_when(
+      diagnosis == 2 ~ "Case",
+      diagnosis == 0 ~ "Control")) %>%
+select(-IID)
+
+w0 <-
+data %>%
+filter(wave == "W0") %>%
+ggplot(aes(PRS, fill = diagnosis, color = diagnosis)) +
+geom_density(alpha = 0.2, size = 1) +
+scale_fill_manual(values = c("red", "blue")) +
+theme_publish() +
+theme(
+  axis.line.x = element_line(linewidth = 1),
+  axis.line.y = element_line(linewidth = 1),
+  text = element_text(size = 7),
+  axis.text = element_text(size = 7)) +
+  labs(y = "\n", x = "")
+
+w1 <-
+data %>%
+filter(wave == "W1") %>%
+ggplot(aes(PRS, fill = diagnosis, color = diagnosis)) +
+geom_density(alpha = 0.2, size = 1) +
+scale_fill_manual(values = c("red", "blue")) +
+theme_publish() +
+theme(
+  axis.line.x = element_line(linewidth = 1),
+  axis.line.y = element_line(linewidth = 1),
+  text = element_text(size = 7),
+  axis.text = element_text(size = 7)) +
+  labs(y = "\nDensity", x = "")
+
+w2 <-
+data %>%
+filter(wave == "W1") %>%
+ggplot(aes(PRS, fill = diagnosis, color = diagnosis)) +
+geom_density(alpha = 0.2, size = 1) +
+scale_fill_manual(values = c("red", "blue")) +
+theme_publish() +
+theme(
+  axis.line.x = element_line(linewidth = 1),
+  axis.line.y = element_line(linewidth = 1),
+  text = element_text(size = 7),
+  axis.text = element_text(size = 7)) +
+  labs(y = "\n", x = "PRS")
+
+library(ggpubr)
+
+final <-
+ggarrange(
+  w0, w1, w2, nrow = 3, labels = c("A", "B", "C"),
+  font.label = list(size = 7, color = "black", face = "bold", family = "Arial"),
+  common.legend = TRUE)
+
+ggsave(
+  "Figure4.png", final, device = "png",
+  width = 85, height = 100, units = c("mm"),
+  dpi = 300, bg = "white")
