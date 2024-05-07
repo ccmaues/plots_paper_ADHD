d062f868126513201702b3118e05f3e7e083da3b
ccmaues <ccmaues@gmail.com>
2024-05-07 14:53:09 -0300

    Normality and bias check

--- table3.R
+++ table3.R
@@ -24,28 +24,45 @@ data <-
 
 library(car)
 library(rstatix)
-## T test for two samples
 
+# incluenciates the SD, SE and CI if not unique.
+non_redudant_data <-
+  data %>%
+  select(PRS, sex, popID) %>%
+  unique()
+
+non_redudant_data %>%
+  group_by(sex) %>%
+  get_summary_stats(PRS, type = "common")
+
+non_redudant_data  %>%
+  group_by(popID) %>%
+  get_summary_stats(PRS, type = "common")
+
+## T test for two samples
+# without wave
 # NORMALITY:
-data %>%
-  group_by(wave, diagnosis, sex) %>%
+non_redudant_data %>%
+  group_by(sex) %>%
   shapiro_test(PRS)
 
-data %>%
-  group_by(wave, diagnosis, popID) %>%
+non_redudant_data %>%
+  group_by(sex) %>%
+  
+ks.test(filter(non_redudant_data, sex == "Female")$PRS, rnorm(1363))
+
+non_redudant_data %>%
+  group_by(popID) %>%
   shapiro_test(PRS)
 
 # EQUAL VARIANCES:
-levene_test(data, PRS ~ sex, center = "mean")
-levene_test(data, PRS ~ popID, center = "mean")
-
-# INDEPEDENCE:
-# the samples are independent
+levene_test(non_redudant_data, PRS ~ sex, center = "mean")
+levene_test(non_redudant_data, PRS ~ popID, center = "mean")
 
 # T-TEST
-t_test(data, PRS ~ sex, var.equal = TRUE, detailed = TRUE)
-t_test(data, PRS ~ popID, var.equal = TRUE, detailed = TRUE)
+t_test(non_redudant_data, PRS ~ sex)
+t_test(non_redudant_data, PRS ~ popID)
 
-# COHEN D
-cohens_d(data, PRS ~ sex, var.equal = TRUE)
-cohens_d(data, PRS ~ popID, var.equal = TRUE)
+# EFFECt SIZE
+cohens_d(non_redudant_data, PRS ~ sex, var.equal = TRUE)
+cohens_d(non_redudant_data, PRS ~ popID, var.equal = TRUE)
