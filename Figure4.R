b4606fb45d433e8f7d7beeaa3a0ddb66a079e81e
ccmaues <ccmaues@gmail.com>
2024-05-07 13:17:20 -0300

    Changed function for density plots

--- Figure4.R
+++ Figure4.R
@@ -24,51 +24,55 @@ plyr::join_all(
       diagnosis == 0 ~ "Control")) %>%
 select(-IID)
 
+library(ggpubr)
+ggthemr("dust")
 w0 <-
 data %>%
 filter(wave == "W0") %>%
-ggplot(aes(PRS, fill = diagnosis, color = diagnosis)) +
-geom_density(alpha = 0.2) +
-scale_fill_manual(values = c("red", "blue")) +
-theme_publish() +
-theme(
-  text = element_text(size = 7),
-  axis.text = element_text(size = 7)) +
-  labs(y = "\n", x = "")
+ggdensity(
+  x = "PRS", add = "mean", fill = "diagnosis", rug = TRUE,
+  color = "diagnosis", palette = c("#ad5478b4", "#1c4f5ec7")) +
+  labs(x = "", y = "") +
+  theme_publish() +
+  theme(
+    axis.line.y = element_line(linewidth = 0.5, color = "black"),
+    axis.line.x = element_line(linewidth = 0.5, color = "black")
+  )
 
 w1 <-
 data %>%
 filter(wave == "W1") %>%
-ggplot(aes(PRS, fill = diagnosis, color = diagnosis)) +
-geom_density(alpha = 0.2) +
-scale_fill_manual(values = c("red", "blue")) +
-theme_publish() +
-theme(
-  text = element_text(size = 7),
-  axis.text = element_text(size = 7)) +
-  labs(y = "\nDensity", x = "")
+ggdensity(
+  x = "PRS", add = "mean", fill = "diagnosis", rug = TRUE,
+  color = "diagnosis", palette = c("#ad5478b4", "#1c4f5ec7")) +
+  labs(x = "", y = "Density") +
+  theme_publish() +
+  theme(
+    axis.line.y = element_line(linewidth = 0.5, color = "black"),
+    axis.line.x = element_line(linewidth = 0.5, color = "black")
+  )
 
 w2 <-
 data %>%
 filter(wave == "W1") %>%
-ggplot(aes(PRS, fill = diagnosis, color = diagnosis)) +
-geom_density(alpha = 0.2) +
-scale_fill_manual(values = c("red", "blue")) +
-theme_publish() +
-theme(
-  text = element_text(size = 7),
-  axis.text = element_text(size = 7)) +
-  labs(y = "\n", x = "PRS")
-
+ggdensity(
+  x = "PRS", add = "mean", fill = "diagnosis", rug = TRUE,
+  color = "diagnosis", palette = c("#ad5478b4", "#1c4f5ec7")) +
+  labs(x = "PRS z-score", y = "") +
+  theme_publish() +
+  theme(
+    axis.line.y = element_line(linewidth = 0.5, color = "black"),
+    axis.line.x = element_line(linewidth = 0.5, color = "black")
+  )
 library(ggpubr)
 
 final <-
 ggarrange(
   w0, w1, w2, nrow = 3, labels = c("A", "B", "C"),
-  font.label = list(size = 7, color = "black", face = "bold", family = "Arial"),
+  font.label = list(size = 10, color = "black", face = "bold", family = "Arial"),
   common.legend = TRUE)
 
 ggsave(
-  "Figure4_version2.png", final, device = "png",
-  width = 85, height = 100, units = c("mm"),
+  "Figure4_version3.png", final, device = "png",
+  width = 85, height = 180, units = c("mm"),
   dpi = 300, bg = "white")
