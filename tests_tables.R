3e87aa9d79e2b081787dea44692df4f0824b93ea
ccmaues <ccmaues@gmail.com>
2024-05-13 19:42:35 -0300

    I legit think i am losing my mind with this phd

--- tests_tables.R
+++ tests_tables.R
@@ -2,31 +2,16 @@
 source("data_to_source.R")
 source("functions_to_source.R")
 
-## For all observations (no time var) FALTA A IDADEEE
-no_duplicate <-
+## For all observations (no time var)
+# I argue that because all samples appear 3 times,
+# it won't be an issue ni the GLM, cuz its almost
+# controled...? I mean, in ALL samples.
+all_time <-
   data %>%
-  select(-age, -wave) %>%
-  unique() %>%
   mutate(
     new_risk = add_risk(., "adjusted_PRS"),
     old_risk = add_risk(., "original_PRS")) %>%
   select(-IID)
-
-for_age <-
-  data %>%
-  select(diagnosis, age)
-
-age_fem <-
-  data %>%
-  filter(sex == "Female") %>%
-  select(diagnosis, age) %>%
-  rename(fem_age = age)
-
-age_man <-
-  data %>%
-  filter(sex == "Male") %>%
-  select(diagnosis, age) %>%
-  rename(man_age = age)
 
 ## For time comparison
 W0 <-
@@ -57,12 +42,7 @@ W2 <-
   select(-IID)
 
 ## Get Odds Ratios into object
-all_time_model <-
-  uni_model_cal(no_duplicate) %>%
-  bind_rows(
-    uni_model_cal(for_age),
-    uni_model_cal(age_fem),
-    uni_model_cal(age_man))
+all_time_model <- uni_model_cal(all_time)
 W0_model <- uni_model_cal(W0)
 W1_model <- uni_model_cal(W1)
 W2_model <- uni_model_cal(W2)
@@ -71,7 +51,8 @@ W2_model <- uni_model_cal(W2)
 mod <-
   all_time_model %>%
   edit() %>%
-  filter(term != "(Intercept)" & term != "x")
+  filter(term != "(Intercept)" & term != "x") %>%
+  arrange(term)
 # saveRDS(mod, "cass_BHRC_OR_univariated_model.R")
 # mod <- readRDS("cass_BHRC_OR_univariated_model.R")
 # seria legal colocar a idade separada por sexo também
@@ -81,7 +62,7 @@ mod <-
 #     c("Female", "Male", "Rio Grande do Sul", "São Paulo",
 #     "PRS adjusted", "PRS original", "Parents' diagnosis", "Age"))
 
-# mod <- with(mod, mod[order(term),])
+mod$term <- factor(mod$term, levels = paste(mod$term, sep = ","))
 
 library(stringr)
 library(forcats)
@@ -108,14 +89,19 @@ mod2 <-
     as.character(round(p.value, 2)),
     width = 4,
     pad = "0",
-    side = "right"))) %>%
-  bind_rows(
-    data.frame(
-    term = "Variable",
-    OR_CI = "Hazard Ratio (95% CI)",
-    conf.low = "",
-    conf.high = "",
-    p.value = "p-value"))
+    side = "right")))
+  # bind_rows(
+  #   data.frame(
+  #   term = "Variable",
+  #   OR_CI = "Hazard Ratio (95% CI)",
+  #   conf.low = "",
+  #   conf.high = "",
+  #   p.value = "p-value"))
+
+mod2$term <-
+  factor(mod2$term,
+  levels = c("Variable", "Age", "Female", "Male", "PRS adjusted", "PRS original", "Parents' diagnosis",
+  "Rio Grande do Sul", "São Paulo", "W0", "W1", "W2"))
 
 middle <-
   ggplot(mod, aes(estimate, term)) +
@@ -123,15 +109,16 @@ middle <-
   geom_vline(xintercept = 0, linetype = "dashed", linewidth = 0.5, color = "#3a3a3a") +
   geom_point(shape = 15, size = 3, color = "#013464") +
   labs(x = "Odds Ratio [95% CI]", y = "") +
-  coord_cartesian(ylim = c(1, 11), xlim = c(0, 4.5)) +
+  coord_cartesian(ylim = c(1, 11), xlim = c(0, 3.5)) +
   theme_publish() +
   theme(
-    axis.line.y = element_blank(),
-    axis.ticks.y = element_blank(),
-    axis.text.y = element_blank(),
-    axis.title.y = element_blank(),
     axis.line.x = element_line(linewidth = 0.5, color = "black"),
-    axis.title.x = element_text(face = "bold"))
+    axis.line.y = element_line(linewidth = 0.5, color = "black"),
+    axis.title.x = element_text(face = "bold"),
+    axis.title.y = element_text(face = "bold"),
+    axis.text.y = element_text(color = "black"),
+    axis.text.x = element_text(color = "black")
+    )
 
 left <-
   ggplot(mod2, aes(x = 0, label = term, y = term)) +
@@ -141,11 +128,16 @@ left <-
   coord_cartesian(xlim = c(0, 4)) +
   theme_void()
 
+# right <-
+#   ggplot(mod2) +
+#   geom_text(
+#     aes(x = 0, y = term, label = p.value), hjust = 0,
+#     fontface = ifelse(mod2$p.value == "p-value", "bold", "plain")) +
+#   theme_void()
+
 right <-
   ggplot(mod2) +
-  geom_text(
-    aes(x = 0, y = term, label = p.value), hjust = 0,
-    fontface = ifelse(mod2$p.value == "p-value", "bold", "plain")) +
+  geom_text(aes(x = 0, y = term, label = p.value), hjust = 0) +
   theme_void()
 
 library(patchwork)
@@ -155,6 +147,13 @@ layout <- c(
   area(t = 1, l = 5, b = 30, r = 11), # middle plot
   area(t = 0, l = 11, b = 30, r = 12) # right plot
 )
+
+layout <- c(
+  area(t = 1, l = 1, b = 30, r = 9), # middle plot
+  area(t = 0, l = 8, b = 30, r = 12) # right plot
+)
+middle + right + plot_layout(design = layout)
+
 left + middle + right + plot_layout(design = layout)
 
 ggsave("forest-plot2.png", units = "mm", width = 160, height = 175)
@@ -170,24 +169,28 @@ ggsave("forest-plot2.png", units = "mm", width = 160, height = 175)
 #   )
 
 ## Get Odds Ratio into a table
-# all_time_OR <- uni_model_OR(no_duplicate)
-# W0_OR <- uni_model_OR(W0)
-# W1_OR <- uni_model_OR(W1)
-# W2_OR <- uni_model_OR(W2)
+all_time_OR <- uni_model_OR(all_time)
+all_time_OR_age <- uni_model_OR(for_age)
+all_time_OR_fem <- uni_model_OR(age_fem)
+all_time_OR_man <- uni_model_OR(age_man)
 
-# all_time_OR %>%
-# as_flex_table() %>%
-# flextable::save_as_docx(fileext = ".docx", path = "all_time_model.docx")
+W0_OR <- uni_model_OR(W0)
+W1_OR <- uni_model_OR(W1)
+W2_OR <- uni_model_OR(W2)
 
-# W0_OR %>%
-# as_flex_table() %>%
-# flextable::save_as_docx(fileext = ".docx", path = "W0_model.docx")
+all_time_OR %>%
+as_flex_table() %>%
+flextable::save_as_docx(fileext = ".docx", path = "all_time_model.docx")
 
-# W1_OR %>%
-# as_flex_table() %>%
-# flextable::save_as_docx(fileext = ".docx", path = "W1_model.docx")
+W0_OR %>%
+as_flex_table() %>%
+flextable::save_as_docx(fileext = ".docx", path = "W0_model.docx")
 
-# W2_OR %>%
-# as_flex_table() %>%
-# flextable::save_as_docx(fileext = ".docx", path = "W2_model.docx")
-# # knitr::kable()
+W1_OR %>%
+as_flex_table() %>%
+flextable::save_as_docx(fileext = ".docx", path = "W1_model.docx")
+
+W2_OR %>%
+as_flex_table() %>%
+flextable::save_as_docx(fileext = ".docx", path = "W2_model.docx")
+# knitr::kable()
